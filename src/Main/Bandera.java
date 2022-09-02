7d53b3a3fe74226fbfeecf3b7eeac6f62612ca44
Walther Carrasco <110209326+walthercarrasco@users.noreply.github.com>
2022-09-02 13:47:07 -0600

    Creacion clase Bandera

---
+++ src/Main/Bandera.java
@@ -0,0 +1,37 @@
+
+package Main;
+
+/**
+ *
+ * @author HP
+ */
+public class Bandera {
+    private String color;
+    private String direccion;
+
+    public Bandera() {
+    }
+
+    public Bandera(String color, String direccion) {
+        this.color = color;
+        this.direccion = direccion;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    
+}
